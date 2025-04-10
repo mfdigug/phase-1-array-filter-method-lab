ae43af1742963ff3358c4e3501f22b086a190b28
Michelle Di Guglielmo <mfdigug@gmail.com>
2025-04-10 15:45:39 +1000

    done with array filter lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution here
+function findMatching(drivers, driverName) {
+  return drivers.filter(function (driver){
+  if (driverName.toLowerCase() === driver.toLowerCase()) { 
+       return driver; 
+     } 
+  });
+}
+
+
+function fuzzyMatch(drivers, someLetters) {
+return drivers.filter(function (driver) {
+  if (driver.startsWith(someLetters)) { 
+    return driver; 
+  } 
+});
+}
+
+
+
+function matchName(drivers, name) {
+
+  return drivers.filter(function (driver){
+    if (driver.name === name){
+      return driver
+    }
+  })
+}
